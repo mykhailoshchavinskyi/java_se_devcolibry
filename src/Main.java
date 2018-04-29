9e05ba09aae749e309a3576c31e82ac85771363a
Mykhailo Shchavinskyi <mykhailo.shchavinskyi@gmail.com>
2018-04-29 16:14:09 +0300

    Lesson 5. Переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+
+    static byte aByte = 125;
+    static short aShort = 30_006;
+    static int aInt = 2_100_060_550;
+    static long aLong = 9_223_372_036_854_775_807L;
+
+    static float aMinFloat = 0.000_000_000_000_000_000_000_000_000_000_000_000_000_000_001f;
+    static float aMaxFloat = 340_282_000_000_000_000_000_000_000_000_000_000_000.0f;
+
+    static double aMinDouble = 0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001;
+    static double aMaxDouble = 179769300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0;
+
+    static boolean aTrueBoolean = true;
+    static boolean aFalseBoolean = false;
+
+
+
     public static void main(String[] args) {
+
         System.out.println("Hello Devcolibry and Git");
+        System.out.println("aByte = "+aByte);
+        System.out.println("aShort = "+aShort);
+        System.out.println("aInt = "+aInt);
+        System.out.println("aLong = "+aLong);
+        System.out.println("aMinFloat = "+aMinFloat);
+        System.out.println("aMaxFloat = "+aMaxFloat);
+        System.out.println("aMinDouble = "+aMinDouble);
+        System.out.println("aMaxDouble = "+aMaxDouble);
+        System.out.println("aTrueBoolean = "+aTrueBoolean);
+        System.out.println("aFalseBoolean = "+aFalseBoolean);
+
     }
 }
