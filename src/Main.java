acfab4a732d1678ccd996c3a8fbc06c38403adec
Mykhailo Shchavinskyi <mykhailo.shchavinskyi@gmail.com>
2018-04-29 17:00:16 +0300

    Lesson 8. Final variables (CONST)

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,10 @@
 public class Main {
 
-    static byte aByte = 125;
-    static short aShort = 30_006;
-    static int aInt = 2_100_060_550;
-    static long aLong = 9_223_372_036_854_775_807L;
-
-    static float aMinFloat = 0.000_000_000_000_000_000_000_000_000_000_000_000_000_000_001f;
-    static float aMaxFloat = 340_282_000_000_000_000_000_000_000_000_000_000_000.0f;
-
-    static double aMinDouble = 0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001;
-    static double aMaxDouble = 179769300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0;
-
-    static boolean aTrueBoolean = true;
-    static boolean aFalseBoolean = false;
-
-    static String aString = "Hello DevColibri.com";
-    static char aChar = 'D';
-
+    static final double PI = 3.14;
 
     public static void main(String[] args) {
-
-    //  Это однострочный комментарий
-    //  System.out.println("Hello Devcolibry and Git");
-    //  System.out.println("aByte = "+aByte);
-        System.out.println("aShort = "+aShort);
-        System.out.println("aInt = "+aInt);
-
-    /*   Начало многострочного комментария
-
-        System.out.println("aLong = "+aLong);
-        System.out.println("aMinFloat = "+aMinFloat);
-        System.out.println("aMaxFloat = "+aMaxFloat);
-        System.out.println("aMinDouble = "+aMinDouble);
-
-
-         На следующей строчке конец многострочного комментария
-     */
-
-        System.out.println("aMaxDouble = "+aMaxDouble);
-        System.out.println("aTrueBoolean = "+aTrueBoolean);
-        System.out.println("aFalseBoolean = "+aFalseBoolean);
-        System.out.println("aString = "+aString);
-        System.out.println("aChar = "+aChar);
+        System.out.println(PI);
+//        Если раскомментировать будет ошибка
+//        PI = 2*PI;
     }
 }
